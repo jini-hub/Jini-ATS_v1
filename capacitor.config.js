fb9b657a11302c772815b2e6b6b5e7d01febe3a3
jini-hub <woojinzzz@gmail.com>
2026-09-18 11:53:04 +0900

    Delete capacitor.config.js

--- capacitor.config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  appId: 'com.kats.studio',
-  appName: 'Jini-ATS',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  server: { androidScheme: 'https' }
-};
-module.exports = config;
